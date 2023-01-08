1ecdb0c493f1e9f7c2256515908962ebd9ffbc3f
k-karthik7 <kkotha79@gmail.com>
2023-01-09 01:27:08 +0530

    husky install

---
+++ __tests__/testing.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Online Voting tests', () => {
+  beforeAll(() => {})
+  afterAll(() => {})
+  test('First test', () => {
+    expect(true).toBe(true)
+  })
+})
