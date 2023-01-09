28c38dae7c42bcf7b1c302e7d889efa402ceabda
k-karthik7 <kkotha79@gmail.com>
2023-01-09 19:57:52 +0530

    election model added

---
+++ migrations/20230109142518-create-election.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Elections", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      electionName: {
+        type: Sequelize.STRING,
+      },
+      launched: {
+        type: Sequelize.BOOLEAN,
+      },
+      end: {
+        type: Sequelize.BOOLEAN,
+      },
+      publicUrl: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Elections");
+  },
+};
